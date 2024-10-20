88905c1358ce2670fdbcdb4104e5c203f2ba591c
ksolomon <ksolomon@utexas.edu>
2024-10-19 19:27:37 -0700

    add heartbeat

--- Core/Src/freertos.cpp
+++ Core/Src/freertos.cpp
@@ -1,27 +1,28 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * File Name          : freertos.c
-  * Description        : Code for freertos applications
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
 #include "FreeRTOS.h"
-#include "task.h"
-#include "main.h"
+
 #include "cmsis_os.h"
+#include "main.h"
+#include "task.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -55,69 +56,74 @@ typedef StaticEventGroup_t osStaticEventGroupDef_t;
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
+    .name = "defaultTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
+};
+
+osThreadId_t heartbeatTaskHandle;
+const osThreadAttr_t heartbeatTask_attributes = {
+    .name = "heartbeatTask",
+    .stack_size = 128 * 4,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 /* Definitions for myQueue01 */
 osMessageQueueId_t myQueue01Handle;
-uint8_t myQueue01Buffer[ 16 * sizeof( uint16_t ) ];
+uint8_t myQueue01Buffer[16 * sizeof(uint16_t)];
 osStaticMessageQDef_t myQueue01ControlBlock;
-const osMessageQueueAttr_t myQueue01_attributes = {
-  .name = "myQueue01",
-  .cb_mem = &myQueue01ControlBlock,
-  .cb_size = sizeof(myQueue01ControlBlock),
-  .mq_mem = &myQueue01Buffer,
-  .mq_size = sizeof(myQueue01Buffer)
-};
+const osMessageQueueAttr_t myQueue01_attributes = {.name = "myQueue01",
+                                                   .cb_mem = &myQueue01ControlBlock,
+                                                   .cb_size = sizeof(myQueue01ControlBlock),
+                                                   .mq_mem = &myQueue01Buffer,
+                                                   .mq_size = sizeof(myQueue01Buffer)};
 /* Definitions for myTimer01 */
 osTimerId_t myTimer01Handle;
 osStaticTimerDef_t myTimer01ControlBlock;
 const osTimerAttr_t myTimer01_attributes = {
-  .name = "myTimer01",
-  .cb_mem = &myTimer01ControlBlock,
-  .cb_size = sizeof(myTimer01ControlBlock),
+    .name = "myTimer01",
+    .cb_mem = &myTimer01ControlBlock,
+    .cb_size = sizeof(myTimer01ControlBlock),
 };
 /* Definitions for myMutex01 */
 osMutexId_t myMutex01Handle;
 osStaticMutexDef_t myMutex01ControlBlock;
 const osMutexAttr_t myMutex01_attributes = {
-  .name = "myMutex01",
-  .cb_mem = &myMutex01ControlBlock,
-  .cb_size = sizeof(myMutex01ControlBlock),
+    .name = "myMutex01",
+    .cb_mem = &myMutex01ControlBlock,
+    .cb_size = sizeof(myMutex01ControlBlock),
 };
 /* Definitions for myRecursiveMutex01 */
 osMutexId_t myRecursiveMutex01Handle;
 osStaticMutexDef_t myRecursiveMutex01ControlBlock;
 const osMutexAttr_t myRecursiveMutex01_attributes = {
-  .name = "myRecursiveMutex01",
-  .attr_bits = osMutexRecursive,
-  .cb_mem = &myRecursiveMutex01ControlBlock,
-  .cb_size = sizeof(myRecursiveMutex01ControlBlock),
+    .name = "myRecursiveMutex01",
+    .attr_bits = osMutexRecursive,
+    .cb_mem = &myRecursiveMutex01ControlBlock,
+    .cb_size = sizeof(myRecursiveMutex01ControlBlock),
 };
 /* Definitions for myBinarySem01 */
 osSemaphoreId_t myBinarySem01Handle;
 osStaticSemaphoreDef_t myBinarySem01ControlBlock;
 const osSemaphoreAttr_t myBinarySem01_attributes = {
-  .name = "myBinarySem01",
-  .cb_mem = &myBinarySem01ControlBlock,
-  .cb_size = sizeof(myBinarySem01ControlBlock),
+    .name = "myBinarySem01",
+    .cb_mem = &myBinarySem01ControlBlock,
+    .cb_size = sizeof(myBinarySem01ControlBlock),
 };
 /* Definitions for myCountingSem01 */
 osSemaphoreId_t myCountingSem01Handle;
 osStaticSemaphoreDef_t myCountingSem01ControlBlock;
 const osSemaphoreAttr_t myCountingSem01_attributes = {
-  .name = "myCountingSem01",
-  .cb_mem = &myCountingSem01ControlBlock,
-  .cb_size = sizeof(myCountingSem01ControlBlock),
+    .name = "myCountingSem01",
+    .cb_mem = &myCountingSem01ControlBlock,
+    .cb_size = sizeof(myCountingSem01ControlBlock),
 };
 /* Definitions for myEvent01 */
 osEventFlagsId_t myEvent01Handle;
 osStaticEventGroupDef_t myEvent01ControlBlock;
 const osEventFlagsAttr_t myEvent01_attributes = {
-  .name = "myEvent01",
-  .cb_mem = &myEvent01ControlBlock,
-  .cb_size = sizeof(myEvent01ControlBlock),
+    .name = "myEvent01",
+    .cb_mem = &myEvent01ControlBlock,
+    .cb_size = sizeof(myEvent01ControlBlock),
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -126,103 +132,110 @@ const osEventFlagsAttr_t myEvent01_attributes = {
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
+void StartHeartbeatTask(void *argument);
 void Callback01(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
 void MX_FREERTOS_Init(void) {
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
-  /* Create the mutex(es) */
-  /* creation of myMutex01 */
-  myMutex01Handle = osMutexNew(&myMutex01_attributes);
+    /* USER CODE END Init */
+    /* Create the mutex(es) */
+    /* creation of myMutex01 */
+    myMutex01Handle = osMutexNew(&myMutex01_attributes);
 
-  /* Create the recursive mutex(es) */
-  /* creation of myRecursiveMutex01 */
-  myRecursiveMutex01Handle = osMutexNew(&myRecursiveMutex01_attributes);
+    /* Create the recursive mutex(es) */
+    /* creation of myRecursiveMutex01 */
+    myRecursiveMutex01Handle = osMutexNew(&myRecursiveMutex01_attributes);
 
-  /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
+    /* USER CODE BEGIN RTOS_MUTEX */
+    /* add mutexes, ... */
+    /* USER CODE END RTOS_MUTEX */
 
-  /* Create the semaphores(s) */
-  /* creation of myBinarySem01 */
-  myBinarySem01Handle = osSemaphoreNew(1, 1, &myBinarySem01_attributes);
+    /* Create the semaphores(s) */
+    /* creation of myBinarySem01 */
+    myBinarySem01Handle = osSemaphoreNew(1, 1, &myBinarySem01_attributes);
 
-  /* creation of myCountingSem01 */
-  myCountingSem01Handle = osSemaphoreNew(2, 0, &myCountingSem01_attributes);
+    /* creation of myCountingSem01 */
+    myCountingSem01Handle = osSemaphoreNew(2, 0, &myCountingSem01_attributes);
 
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
+    /* USER CODE BEGIN RTOS_SEMAPHORES */
+    /* add semaphores, ... */
+    /* USER CODE END RTOS_SEMAPHORES */
 
-  /* Create the timer(s) */
-  /* creation of myTimer01 */
-  myTimer01Handle = osTimerNew(Callback01, osTimerPeriodic, NULL, &myTimer01_attributes);
+    /* Create the timer(s) */
+    /* creation of myTimer01 */
+    myTimer01Handle = osTimerNew(Callback01, osTimerPeriodic, NULL, &myTimer01_attributes);
 
-  /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
+    /* USER CODE BEGIN RTOS_TIMERS */
+    /* start timers, add new ones, ... */
+    /* USER CODE END RTOS_TIMERS */
 
-  /* Create the queue(s) */
-  /* creation of myQueue01 */
-  myQueue01Handle = osMessageQueueNew (16, sizeof(uint16_t), &myQueue01_attributes);
+    /* Create the queue(s) */
+    /* creation of myQueue01 */
+    myQueue01Handle = osMessageQueueNew(16, sizeof(uint16_t), &myQueue01_attributes);
 
-  /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
+    /* USER CODE BEGIN RTOS_QUEUES */
+    /* add queues, ... */
+    /* USER CODE END RTOS_QUEUES */
 
-  /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+    /* Create the thread(s) */
+    /* creation of defaultTask */
+    defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+    heartbeatTaskHandle = osThreadNew(StartHeartbeatTask, NULL, &heartbeatTask_attributes);
 
-  /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
-  /* USER CODE END RTOS_THREADS */
+    /* USER CODE BEGIN RTOS_THREADS */
+    /* add threads, ... */
+    /* USER CODE END RTOS_THREADS */
 
-  /* creation of myEvent01 */
-  myEvent01Handle = osEventFlagsNew(&myEvent01_attributes);
+    /* creation of myEvent01 */
+    myEvent01Handle = osEventFlagsNew(&myEvent01_attributes);
 
-  /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
-
+    /* USER CODE BEGIN RTOS_EVENTS */
+    /* add events, ... */
+    /* USER CODE END RTOS_EVENTS */
 }
 
 /* USER CODE BEGIN Header_StartDefaultTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
-  /* USER CODE BEGIN StartDefaultTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END StartDefaultTask */
+void StartDefaultTask(void *argument) {
+    /* USER CODE BEGIN StartDefaultTask */
+    /* Infinite loop */
+    for (;;) {
+        osDelay(1);
+    }
+    /* USER CODE END StartDefaultTask */
+}
+void StartHeartbeatTask(void *argument) {
+    /* USER CODE BEGIN StartDefaultTask */
+    /* Infinite loop */
+    HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+    for (;;) {
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+        osDelay(1000);
+    }
+    /* USER CODE END StartDefaultTask */
 }
 
 /* Callback01 function */
-void Callback01(void *argument)
-{
-  /* USER CODE BEGIN Callback01 */
+void Callback01(void *argument) {
+    /* USER CODE BEGIN Callback01 */
 
-  /* USER CODE END Callback01 */
+    /* USER CODE END Callback01 */
 }
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
 
 /* USER CODE END Application */
-
